27c02f2262578144a30159ee0b641423c5677989
axiomcura <erikishere3@gmail.com>
2021-10-20 16:33:37 -0600

    assignment2 init

---
+++ assignments/assignment_2/net.py
@@ -0,0 +1,13 @@
+# this is where the main script of the subnet work will be written
+# curretnyl the script is beeing written as a notebook 
+import sys
+import argparse 
+
+
+def help():
+    pass
+
+if __name__ == '__main__':
+
+	# CLI Arguments will 
+	pass
